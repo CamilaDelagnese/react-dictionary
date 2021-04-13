46e7a611102fa3c4970c2d4c306ac27ad343efe4
CamilaDelagnese <camiladelagnese22@gmail.com>
2021-04-13 18:48:46 +0100

    add searching engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-     
-        Dictionary App !!!
+ 
+      Dictionary App 
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+       <small> Coded by Camila Delagnese</small>
+      </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
